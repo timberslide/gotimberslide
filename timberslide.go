5a40702b0803041c1252e17e4f01645c8962a375
Kris Foster <kris.foster@gmail.com>
2016-11-19 13:24:22 -0800

    Push logic back up to the client (#3)
    * Create a new client without a config file
    
    * factor out connection establishment
    
    * factor out presentation layer stuff for Get() into the client
    
    * move connection establishment into the client
    
    * no need for addTokenToContext to be public
    
    * move Close near Connect
    
    * not sure how those got there
    
    * wip: use channels for sending events
    
    * don't fool around with channels
    
    * strip out logging, doesn't belong here
    
    * trash client.send, let the user of the package do this

--- timberslide.go
+++ timberslide.go
@@ -1,11 +1,9 @@
 package ts
 
 import (
-	"bufio"
 	"fmt"
 	"io/ioutil"
 	"net"
-	"os"
 	"os/user"
 
 	"github.com/BurntSushi/toml"
@@ -26,10 +24,19 @@ const (
 type Client struct {
 	Host  string
 	Token string
+	conn  *grpc.ClientConn
 }
 
-// NewClient creates a client from the config file
-func NewClient(file string) (Client, error) {
+// NewClient creates a new client
+func NewClient(host string, token string) (Client, error) {
+	var client Client
+	client.Host = host
+	client.Token = token
+	return client, nil
+}
+
+// NewClientFromFile creates a client from the config file
+func NewClientFromFile(file string) (Client, error) {
 	var client Client
 
 	usr, err := user.Current()
@@ -49,83 +56,101 @@ func NewClient(file string) (Client, error) {
 	return client, nil
 }
 
-// AddTokenToContext adds a bearer token to the given context
-func AddTokenToContext(ctx context.Context, token string) context.Context {
+// addTokenToContext adds a bearer token to the given context
+func addTokenToContext(ctx context.Context, token string) context.Context {
 	md := metadata.Pairs("ts-access-token", token)
 	ctx = metadata.NewContext(ctx, md)
 	return ctx
 }
 
-// Send takes stdin and sends it to the topic
-func (c *Client) Send(topic string) error {
-	fmt.Fprintf(os.Stderr, "ts: connecting...\n")
+// Connect connects to Timberslide
+func (c *Client) Connect() error {
 	host, _, err := net.SplitHostPort(c.Host)
 	if err != nil {
 		return err
 	}
 	creds := credentials.NewClientTLSFromCert(nil, host)
-	conn, err := grpc.Dial(c.Host, grpc.WithTransportCredentials(creds), grpc.WithBlock())
+	c.conn, err = grpc.Dial(c.Host, grpc.WithTransportCredentials(creds), grpc.WithBlock())
 	if err != nil {
 		return err
 	}
-	defer conn.Close()
-
-	client := NewIngestClient(conn)
-	ctx := AddTokenToContext(context.Background(), c.Token)
-	stream, err := client.StreamEvents(ctx)
-	if err != nil {
-		return err
-	}
-
-	msg := &Event{Topic: topic}
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		msg.Message = scanner.Text()
-		// TODO make printing to stdout optional
-		fmt.Println(msg.Message)
-		if err = stream.Send(msg); err != nil {
-			return err
-		}
-	}
-	if err = scanner.Err(); err != nil {
-		return err
-	}
-	// We finished normally, so...
-	// set the done flag and block until we hear something from the server
-	if err = stream.Send(&Event{Topic: topic, Done: true}); err != nil {
-		return err
-	}
-	fmt.Fprintf(os.Stderr, "ts: done, waiting for server...\n")
-	if _, err = stream.Recv(); err != nil {
-		return err
-	}
-	err = stream.CloseSend()
-
-	return err
+	return nil
 }
 
-// Get receives the stream from the topic and writes it to stdout
-func (c *Client) Get(topic string, position int64) error {
-	host, _, err := net.SplitHostPort(c.Host)
+// Close closes the client's connection to Timberslide
+func (c *Client) Close() error {
+	return c.conn.Close()
+}
+
+// Channel is used to send messages over
+type Channel struct {
+	Topic  string
+	client IngestClient
+	stream Ingest_StreamEventsClient
+}
+
+// CreateChannel returns a channel on which the client can send events
+func (c *Client) CreateChannel(topic string) (Channel, error) {
+	var err error
+	channel := Channel{
+		Topic: topic,
+	}
+	channel.client = NewIngestClient(c.conn)
+	ctx := addTokenToContext(context.Background(), c.Token)
+	channel.stream, err = channel.client.StreamEvents(ctx)
+	if err != nil {
+		return channel, err
+	}
+	return channel, nil
+}
+
+// Send sends a message for the channel's topic
+func (c *Channel) Send(message string) error {
+	if err := c.stream.Send(&Event{Topic: c.Topic, Message: message}); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Close closes out the channel with the server
+func (c *Channel) Close() error {
+	var err error
+	if err = c.stream.Send(&Event{Topic: c.Topic, Done: true}); err != nil {
+		return err
+	}
+	if _, err = c.stream.Recv(); err != nil {
+		return err
+	}
+	err = c.stream.CloseSend()
 	if err != nil {
 		return err
 	}
-	creds := credentials.NewClientTLSFromCert(nil, host)
-	conn, err := grpc.Dial(c.Host, grpc.WithTransportCredentials(creds))
-	if err != nil {
-		return err
-	}
-	client := NewStreamerClient(conn)
-	ctx := AddTokenToContext(context.Background(), c.Token)
-	stream, err := client.GetStream(ctx, &Topic{Name: topic, Position: position})
-	if err != nil {
-		return err
-	}
-	for {
-		event, err := stream.Recv()
+	return nil
+}
+
+// Iter providers an iterator to range over event in a topic
+func (c *Client) Iter(topic string, position int64) <-chan Event {
+	ch := make(chan Event)
+	go func() {
+		client := NewStreamerClient(c.conn)
+		ctx := addTokenToContext(context.Background(), c.Token)
+		stream, err := client.GetStream(ctx, &Topic{Name: topic, Position: position})
 		if err != nil {
-			return err
+			return
 		}
-		fmt.Println(event.Message)
-	}
+		for {
+			event, err := stream.Recv()
+			if err != nil {
+				break
+			}
+			ch <- *event
+		}
+	}()
+	return ch
+}
+
+// GetTopics returns a list of all topics visible to this user
+func (c *Client) GetTopics() ([]string, error) {
+	// Implement me
+	return nil, nil
 }
